15bcca49ef6426ee51d40483f71a662955442aed
rahoofard <rahoofard@mail.csuchico.edu>
2018-09-24 21:20:46 -0700

    Here I prompted the user for their first and last name and according to what they put down as their name it gave them a fortune.  This depended on the position of consenants and vowels in their names.

--- main.cpp
+++ main.cpp
@@ -14,8 +14,25 @@ cout<<"What is your first name?"<< endl;
 cin>>first;
 cout<<"What is your last name?"<< endl;
 cin>>last;
-
+//string word = first;
+//string word2 = last;
+string nickname = "";
+nickname += first[0];
+nickname += '.';
+nickname += last[0];
+nickname += '.';
+cout<<"Welcome, " <<nickname<< ", here is your fortune..."<<endl;
+cout<<"your lucky number is " <<first.length()<<endl;
   //tell fortune
+if (first[0] ==  'a' || first[0] ==  'e' || first[0] == 'i' || first[0] == 'o' || first[0] ==  'u' || first[0] ==  'A' || first[0] ==  'E' || first[0] == 'I' || first[0] == 'O' || first[0] ==  'U')
+cout<< "you are destined to be famous!"<<endl;
+else{ 
+cout<<"you should keep a low profile"<<endl;
+}
+int index = last.length()-1;
+if ( last[index] == 'a' || last[index] == 'e'  || last[index] == 'i' || last[index] == 'o' || last[index] == 'u'|| last[index] == 'A' || last[index] == 'E'  || last[index] == 'I' || last[index] == 'O' || last[index] == 'U')
+cout<< "you have already met your true love."<<endl;
 
+cout<<"have a good day!"<<endl;
   return 0;
 }
