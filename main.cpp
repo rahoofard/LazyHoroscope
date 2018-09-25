d58ad58f66bf8cb48c818910c75fa122de7941aa
rahoofard <rahoofard@mail.csuchico.edu>
2018-09-24 21:23:59 -0700

    Here I prompted the user for their first and last name and according to what they put down as their name it gave them a fortune.  This depended on the position of consenants and vowels in their names.

--- main.cpp
+++ main.cpp
@@ -27,7 +27,7 @@ cout<<"your lucky number is " <<first.length()<<endl;
 if (first[0] ==  'a' || first[0] ==  'e' || first[0] == 'i' || first[0] == 'o' || first[0] ==  'u' || first[0] ==  'A' || first[0] ==  'E' || first[0] == 'I' || first[0] == 'O' || first[0] ==  'U')
 cout<< "you are destined to be famous!"<<endl;
 else{ 
-cout<<"you should keep a low profile"<<endl;
+cout<<"you should keep a low profile."<<endl;
 }
 int index = last.length()-1;
 if ( last[index] == 'a' || last[index] == 'e'  || last[index] == 'i' || last[index] == 'o' || last[index] == 'u'|| last[index] == 'A' || last[index] == 'E'  || last[index] == 'I' || last[index] == 'O' || last[index] == 'U')
